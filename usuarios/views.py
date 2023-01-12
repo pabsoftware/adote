e90db9997dbd67843e1e28e2bbb392a953353d13
paulopab <pabsoftware@outlook.com>
2023-01-12 09:32:07 -0300

    estilizando cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def cadastro(request):
-    template_name = 'cadastro.html'
+    template_name = 'usuarios/cadastro.html'
     return render(request, template_name )
