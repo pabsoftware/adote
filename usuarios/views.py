72f6bcf510f8ca5f4565987d660081610faa8dbb
pabsoftware <yopabsoftware@outlook.com>
2023-01-14 09:35:20 -0300

    Iplementaçõa da views cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -5,4 +5,22 @@ from django.http import HttpResponse
 
 def cadastro(request):
     template_name = 'usuarios/cadastro.html'
-    return render(request, template_name )
+    if request.method == 'GET':
+        
+        return render(request, template_name)
+    elif request.method == 'POST':
+    
+        nome    = request.POST.get('nome')
+        email   = request.POST.get('email')
+        senha   = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+       
+        if senha != confirmar_senha:
+            return render(request, template_name)
+
+        if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
+            return HttpResponse('Todos os campos são de preenchimento obrigatorio') 
+        
+        else:
+            return render(request, template_name)
+            #return HttpResponse(f'{nome}, {email}, {senha}, {confirmar_senha}')
