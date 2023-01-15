cfff3aef2383149eb4c3393563c71ebe13f45051
pabsoftware <yopabsoftware@outlook.com>
2023-01-14 22:22:34 -0300

    criado view novo_pet e  seu template

--- divulgar/models.py
+++ divulgar/models.py
@@ -30,7 +30,7 @@ class Pet(models.Model):
     telefone = models.CharField(max_length=15)
     raca = models.ForeignKey(Raca, on_delete=models.DO_NOTHING)
     tags = models.ManyToManyField(Tag)
-    status = models.CharField(max_length=1, choices= choices_status)
+    status = models.CharField(max_length=1, choices= choices_status, default='P')
 
     def __str__(self):
         return self.nome
--- divulgar/views.py
+++ divulgar/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Tag, Raca, Pet
+from django.contrib import messages
+from django.contrib.messages import constants
+
 
 
 
@@ -9,9 +13,44 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def novo_pet(request):
     template_name = 'divulgar/novo_pet.html'
+  
     if request.method == 'GET': 
-        return render(request, template_name)
+        tags = Tag.objects.all()
+        racas = Raca.objects.all()
+        context = {'tags': tags, 'racas': racas}
+        return render(request, template_name, context)
     
     elif request.method == 'POST':
-        
-        return render(request, template_name)
+        foto = request.FILES.get('foto')
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        estado = request.POST.get('estado')
+        cidade = request.POST.get('cidade')
+        telefone = request.POST.get('telefone')
+        tags = request.POST.getlist('tags')
+        raca = request.POST.get('raca')
+
+
+        #TODO: Validar dados
+        pet = Pet(
+            usuario=request.user,
+            foto=foto,
+            nome=nome,
+            descricao=descricao,
+            estado=estado,
+            cidade=cidade,
+            telefone=telefone,
+            raca_id=raca,
+        )
+
+        pet.save()
+        for tag_id in tags:
+            tag = Tag.objects.get(id=tag_id)
+            pet.tags.add(tag)
+
+        pet.save()
+        tags = Tag.objects.all()
+        racas = Raca.objects.all()
+        messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
+        context = {'tags': tags, 'racas': racas}
+        return render(request, template_name, context)
